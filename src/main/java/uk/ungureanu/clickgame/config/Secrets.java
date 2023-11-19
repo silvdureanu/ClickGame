ff78cb5ff3f3dd954afd780588963579cbd79459
Silviu Daniel Ungureanu <silviu@ungureanu.uk>
2023-11-20 00:09:46 +0200

    Fix variable typo

--- src/main/java/uk/ungureanu/clickgame/config/Secrets.java
+++ src/main/java/uk/ungureanu/clickgame/config/Secrets.java
@@ -2,8 +2,8 @@ package uk.ungureanu.clickgame.config;
 
 public class Secrets {
     //Authentication token. The game currently assumes a valid connection for the rankings feature to work as expected.
-    private static final String RedisCloudConnectionToken = "secret";
+    private static final String redisCloudConnectionToken = "secret";
     public static String getRedisCloudConnectionToken() {
-        return RedisCloudConnectionToken;
+        return redisCloudConnectionToken;
     }
 }
